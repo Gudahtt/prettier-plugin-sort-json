305f53002738485b4dd3bb11d4bedf78d95e1df1
Mark Stacey <markjstacey@gmail.com>
2020-09-14 15:06:30 -0230

    Add initial plugin implementation
    This plugin adds a preprocesser to sort JSON files.

---
+++ index.ts
@@ -0,0 +1,27 @@
+import { Plugin } from 'prettier';
+import { parsers as babelParsers } from 'prettier/parser-babel';
+
+export default {
+  name: 'prettier-plugin-sort-json',
+  parsers: {
+    'json': {
+      ...babelParsers.json,
+      preprocess (text: any, options: any) {
+        let preprocessedText = text;
+        /* istanbul ignore next */
+        if (babelParsers.json.preprocess) {
+          preprocessedText = babelParsers.json.preprocess(text, options);
+        }
+
+        const json = JSON.parse(preprocessedText);
+
+        const sortedJson: Record<string, any> = {};
+        for (const key of Object.keys(json).sort()) {
+          sortedJson[key] = json[key];
+        }
+
+        return JSON.stringify(sortedJson, null, 2);
+      },
+    },
+  },
+} as Plugin;
