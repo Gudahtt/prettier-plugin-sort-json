93bf1c3adfe6869bf534541cf29e51dddac909f4
Mark Stacey <markjstacey@gmail.com>
2020-09-14 15:08:53 -0230

    Add two simple unit tests

---
+++ index.test.ts
@@ -0,0 +1,40 @@
+import { format } from 'prettier';
+import SortJsonPlugin from '.';
+
+describe('Sort JSON', () => {
+  it('should validate sorted JSON', () => {
+    const fixture = {
+      0: '0',
+      a: 'a',
+      b: 'b',
+      c: 'c',
+    };
+
+    const input = JSON.stringify(fixture, null, 2);
+    const output = format(input, {
+      filepath: 'foo.json',
+      parser: 'json',
+      plugins: [SortJsonPlugin],
+    });
+
+    expect(output).toMatchSnapshot();
+  });
+
+  it('should sort unsorted JSON', () => {
+    const fixture = {
+      0: '0',
+      b: 'b',
+      a: 'a',
+      c: 'c',
+    };
+
+    const input = JSON.stringify(fixture, null, 2);
+    const output = format(input, {
+      filepath: 'foo.json',
+      parser: 'json',
+      plugins: [SortJsonPlugin],
+    });
+
+    expect(output).toMatchSnapshot();
+  });
+});
